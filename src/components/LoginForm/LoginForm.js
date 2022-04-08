62b406e748e8e9ea0f1cbd884f68f566d2e1c4dc
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-08 11:51:52 +0600

    added register & login handler

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,52 +1,87 @@
 import Button from 'react-bootstrap/Form';
-import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import './LoginForm.css';
 import { MdOutlineMail } from 'react-icons/md';
 import { FcGoogle } from 'react-icons/fc';
-import { getAuth, signInWithPopup } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from 'firebase/auth';
 import app from '../../firebase.init';
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider } from 'firebase/auth';
+import { useState } from 'react';
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-
+const provider = new GoogleAuthProvider();
 
 const LoginForm = () => {
-  const [email, setEmail] = useState('')
-  const [pass, setPass] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [registered, setRegistered] = useState(false)
 
-  const provider = new GoogleAuthProvider();
-
-  //handleGoogle
-  const handleGoogle = () => {
+  //handle google
+  const handleGoogle = (event) => {
     signInWithPopup(auth, provider)
-    .then(res => {
-      const user = res.user;
-      console.log(user);
-    })
-    .catch(error => {
-      console.error('error', error);
-    })
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.error('error', error);
+      });
+    event.preventDefault();
+  };
+
+  //handle form submit // Registration
+  const handleFormSubmit = (event) => {
+    // console.log(email, password);
+    if(registered){
+      signInWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user
+        console.log(user);
+      })
+      .catch(error => {
+        console.error('error', error);
+      })
+    }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setEmail('')
+        setPassword('')
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+  };
+
+  // verify email{
+    
   }
 
-// handle email
-  const handleEmail = e => {
-    setEmail(e.target.value);
-  } 
+  // handle email
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
-  // handle Pass
-  const handlePass = e => {
-    setPass(e.target.value);
-  }
-
-  // handle submit 
-  const handleSubmit = () => {
-    console.log(email, pass);
-  }
+  //handle password
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
 
+// handle registered 
+const handleRegistered = event => {
+  setRegistered(event.target.checked);
+}
 
   return (
     <div className="form-container">
@@ -54,22 +89,40 @@ const LoginForm = () => {
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control  onBlur={handleEmail} type="email" placeholder="Enter email" />
+            <Form.Control
+              onBlur={handleEmail}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
             <Form.Text className="text-light">
               We'll never share your email with anyone else.
             </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please enter your correct email.
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control onBlur={handlePass} type="password" placeholder="Password" />
+            <Form.Control
+              onBlur={handlePassword}
+              type="password"
+              placeholder="Password"
+              required
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Already a member?" />
+            <Form.Check onChange={handleRegistered} type="checkbox" label="Already a member?" />
           </Form.Group>
 
-          <Button onClick={handleSubmit} className="btn-style" variant="primary" type="submit">
-            Login
+          <Button
+            onClick={handleFormSubmit}
+            className="btn-style"
+            variant="primary"
+            type="submit"
+          >
+            {registered ? 'Login' : 'Register'}
           </Button>
           <p className="text-center py-3">Or Sign in With</p>
           <div className="social-platform d-flex justify-content-around">
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,9 +1,4 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBOz4nAzd8-gupu9N-YEf-T6zAqj1Zr_vk",
   authDomain: "login-form-firebase-auth.firebaseapp.com",
@@ -13,7 +8,6 @@ const firebaseConfig = {
   appId: "1:525673344045:web:db84c9bb9407a09f18dca4"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
